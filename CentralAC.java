8da4d37c005da510c9c54fe76db6e1476d7819a3
minneapolis-edu <minneapolis-edu@users.noreply.github.com>
2015-10-15 10:31:38 -0500

    Update CentralAC.java

--- CentralAC.java
+++ CentralAC.java
@@ -27,7 +27,11 @@ public class CentralAC extends ServiceCall{
     @Override
     public String toString() {
 
-       String resolvedDateString = ( resolvedDate == null) ? "Unresolved" : this.resolvedDate.toString();
+        //These statements use the Java ternary operator
+        //This says "if resolvedDate == null, then set resolvedDate to "Unresolved". Otherwise, set resolvedDate to resolvedDate.toString()
+        //Same logic as an if-else statment, but more consise if the condition is simple and the if statment's task is 
+        //to set the value of a variable based on a condition being true or false. 
+        String resolvedDateString = ( resolvedDate == null) ? "Unresolved" : this.resolvedDate.toString();
         String resolutionString = ( this.resolution == null) ? "Unresolved" : this.resolution;
         String feeString = (fee == UNRESOLVED) ? "Unresolved" : "$" + Double.toString(fee);
 
